4b5b21e4c3f85c9e23889a10962023031958d354
Erick2992 <134516889+Erick2992@users.noreply.github.com>
2023-06-27 11:07:54 -0300

    Add files via upload

---
+++ LocalidadVS/clsLocalidades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class clsLocalidades
+    {
+        public int id_localidad { set; get; }
+        public string nomLocalidad { set; get; }
+        public bool activo { set; get; }
+    }
+}
