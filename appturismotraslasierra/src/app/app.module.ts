08435d4618244fff41009383fe58cee365b24d18
Jennyfer Caucota <caucotafran@gmail.com>
2023-06-29 19:44:10 +0200

    29/06/23
    Volvere a empezar hasta aqui funcional.

--- appturismotraslasierra/src/app/app.module.ts
+++ appturismotraslasierra/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NombreDelComponenteCLIComponent } from './nombre-del-componente-cli/nombre-del-componente-cli.component';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
-import { HttpClient } from "@angular/common/http";
-
 
 @NgModule({
   declarations: [
@@ -19,8 +17,7 @@ import { HttpClient } from "@angular/common/http";
     AppRoutingModule,
     SharedModule,
     PagesModule,
-
-    HttpClient
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
